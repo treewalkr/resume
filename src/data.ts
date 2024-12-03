e3adfcf337ed74679457b0e2d3e8977b541ccd12
treewalkr <suthep@pinroj.com>
2024-12-03 23:57:39 +0700

    Update profile.json

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 export const fullVersionLink = 'https://resume.treewalkr.dev';
-export const sourceLink = 'https://github.com/treewalkr/resume2';
+export const sourceLink = 'https://github.com/treewalkr/resume';
 // TODO: Replace with your own image
 export const ogImageUrl =
 	'https://og-image.vercel.app/Resumette.png?theme=light&md=1&fontSize=200px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-black.svg';
